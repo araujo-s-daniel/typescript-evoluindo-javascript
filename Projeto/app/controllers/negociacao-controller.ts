6cc4865d61f0cac0e340e174c8a6c7dafa134806
Daniel Sena <daniels_araujo@outlook.com.br>
2025-06-12 19:03:44 -0300

    03.12 - Limpando o formulário

--- Projeto/app/controllers/negociacao-controller.ts
+++ Projeto/app/controllers/negociacao-controller.ts
@@ -14,6 +14,7 @@ export class NegociacaoController {
     adiciona(): void {
         const negociacao = this.criaNegociacao();
         console.log(negociacao);
+        this.limparFormulario();
     }
 
     criaNegociacao(): Negociacao {
@@ -23,5 +24,12 @@ export class NegociacaoController {
         const valor = parseFloat(this.inputValor.value);
         const negociacao = new Negociacao(date, quantidade, valor);
         return negociacao;
+    }
+
+    limparFormulario(): void {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
     }
 }
